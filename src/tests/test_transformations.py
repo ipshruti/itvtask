bfbb86b9a81025137d396f30a29eef22907ab8f3
Shruti Priya <pshruti1212@gmail.com>
2022-10-11 23:23:34 +0100

    Add boundaries tests

--- src/tests/test_transformations.py
+++ src/tests/test_transformations.py
@@ -25,7 +25,10 @@ def test_find_daily_hours_watched():
     assert find_daily_hours_watched({"Monthly_number_of_hours_watched": 345.45}) == 11.51
 
 
-def test_find_daily_hours_watched_invalid_no_of_hours():
+@pytest.mark.parametrize(
+    "hours", [-0.1, 744.1]
+)
+def test_find_daily_hours_watched_invalid_no_of_hours(hours):
     with pytest.raises(ValueError) as exc:
-        find_daily_hours_watched({"Monthly_number_of_hours_watched": 899})
+        find_daily_hours_watched({"Monthly_number_of_hours_watched": hours})
     assert str(exc.value) == "Monthly_number_of_hours_watched must be between 0 to 744, inclusive"
